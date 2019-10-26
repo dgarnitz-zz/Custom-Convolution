7a9fd82dee65ef0cd7b67c58d211b2babd04cfae
David Garnitz <david@Davids-MacBook-Pro.local>
2019-10-27 01:00:43 +0200

    method now outputs a series of tensors by building a numpy array at each iteration

--- convolution.py
+++ convolution.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 def convolution(dimension, stride, pixels):
-    #initialize filter
-    filter = np.random.rand(dimension, dimension)
+    #output feature maps
+    features = []
 
-    #grab each channel matrix
+    #initialize filter
+    filtr = np.random.rand(dimension, dimension)
+
+    #variables for convolving
     r1 = 0
     c1 = 0
     r2 = dimension
@@ -15,19 +18,39 @@ def convolution(dimension, stride, pixels):
         c1 = 0
         r2 = dimension
         c2= dimension
+        feature_map=[]
 
         while c2<=len(pixels[0]):
-            patch = pixels[i,c1:c2,r1:r2]
-            dot = np.dot(patch, filter)     
-            print(dot)
+            r1=0
+            r2=dimension
+            row = []
+
+            while r2<=len(pixels[0][0]):
+                patch = pixels[i,c1:c2,r1:r2]
+                dot = np.tensordot(patch, filtr, axes=((0,1),(0,1)))    # patch.ravel().dot(filtr.ravel()) also works 
+                dot1D = np.atleast_1d(dot)  #convert from 0D to 1D array
+                row.append(dot1D[0])
+                r1+=stride
+                r2+=stride
+
+            feature_map.append(row)
             c1+=stride
             c2+=stride
 
+        features.append(feature_map)
 
-    print(len(pixels[0]))
+    features = np.asarray(features)
+    print(features)
     # print(filter.shape==patch.shape)
 
-#you need first the # of channels, iterate over that    
+#Convolutional Layer
+    #NEXT STEP --> append each tensordot to an output list [] feature map 
+    #PROBLEM - weights need to persist for use in backprop
+    
+# Max POOLING
+    #What determines the size of the pooling area? Is it that size of the convolution?
+
+# Zero PADDING   
 
 # the slicing tells it which list to go into, so [k,j,i] would be the 
 # would be in the kth set [[]], in the jth brack [] of k, number i inside j 
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import numpy as np
 #import the image - must be PNG
 # im = imread("../ImageStitch/cropped/left.png")
 im =  np.array([[[1,2,3,4],[5,6,7,8],[9,8,7,6]],[[0,0,0,0],[1,1,1,1],[2,2,2,2]]])
+
 #first convolution
 convolution(2,1,im)
